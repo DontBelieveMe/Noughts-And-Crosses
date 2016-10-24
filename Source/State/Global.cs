e41e05b848b0a0f79d1443e025a501e8448bde53
Barney Wilks <barneywilks1@gmail.com>
2016-10-24 12:33:14 +0100

    Done more structural mono stuff

---
+++ Source/State/Global.cs
@@ -0,0 +1,12 @@
+
+namespace NoughtsAndCrosses
+{
+	public class Global
+	{
+		public static int BoardWidth = 300;
+		public static int BoardHeight = 300;
+		
+		public static int BoardCentre = Global.BoardWidth / 2;
+	}
+}
+
